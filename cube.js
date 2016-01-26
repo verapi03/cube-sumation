0b0effd04d6d65aed9880623b3e00a69ab94c0f6
Andres Vera <raul.andres.vp@gmail.com>
2016-01-25 20:50:38 -0500

    prefix summation and update of the cube diagonal

--- cube.js
+++ cube.js
@@ -6,13 +6,26 @@
 function controller() {
     var input, dataValidated, output;
     input = document.getElementById("input").value;
-    var parsedData = parseData(input)
+    var parsedData = parseInput(input)
     if (parsedData.status) {
+        for (var i = 0; i < parsedData.data.length; i++) {
+            var dimension = parsedData.data[i].dimension;
+            var operations = parseOperation(dimension, parsedData.data[i].queries);
+            // console.log("operations: \n",operations);
+            var matrix = new fenwick(dimension);
+            for (var j = 0; j < operations.length; j++) {
+                if (operations[j].type == 'UPDATE') {
+                    matrix.update(operations[j].x, operations[j].w);
+                } else {
+                    matrix.summation(operations[j].x1, operations[j].x2);
+                }
+            }
+            // fenwick(parsedData.data[i].dimension).createMatrix;
+            // console.log("matrix: \n",matrix.create());
+        }
         output = "Input OK";
-        // console.log(" controller parsedData.data: \n",parsedData.data);
-        createArrow(parsedData.data);
     } else {
-        alert(parseData.error + "\n" + input);
+        alert(parseInput.error + "\n" + input);
         newCalculation();
     }
     document.getElementById("output").innerHTML = output;
@@ -29,8 +42,6 @@ function newCalculation() {
  *  Validates if a variable is a positive integer.
  */
 function isPositiveInteger(num){
-    // console.log("typeof num: ",typeof num);
-    // console.log("num: ",num);
     if (!isNaN(num) || num > 0) {
         return Number(num) % 1 === 0;
     } else {
@@ -44,7 +55,7 @@ function isPositiveInteger(num){
  *  up and returns an object with the information of every operation to be 
  *  calculated over the matrix.
  */
-function parseData(data) {
+function parseInput(data) {
     var response = {'status':true,
             'error': '',
             'data':[]
@@ -54,7 +65,6 @@ function parseData(data) {
         operationsPerTest, 
         index = 1,
         commands = data.split('\n');
-    // console.log("commands: \n", commands);
     var totalTests = commands[0];
     if (!isPositiveInteger(totalTests)) {
         response.status = false;
@@ -66,13 +76,12 @@ function parseData(data) {
     }
     do {
         dimensionAndOperations = commands[index].split(' ');
-        // console.log("parseData dimensionAndOperations: \n", dimensionAndOperations);
         dimension = dimensionAndOperations[0];
         operationsPerTest = dimensionAndOperations[1];
         lastQueryIndex = index + parseInt(operationsPerTest);
         if (!isPositiveInteger(dimension) || !isPositiveInteger(operationsPerTest)) {
             response.status = false;
-            // console.log("parseData response1: \n", response);
+            // console.log("parseInput response1: \n", response);
             return response;
         }
         var testCase = {
@@ -87,11 +96,83 @@ function parseData(data) {
         totalTests--;
     }
     while (totalTests >= 1);
-    // console.log("parseData response2: \n", response);
+    // console.log("parseInput response2: \n", response);
     return response;
 }
 
-function createArrow(testCase) {
-    console.log('testCase: \n',testCase);
-    return true;
+/**
+ *  This function parses the user input queries per test-case.
+ */
+function parseOperation(dimension, queries) {
+    var operation = [];
+    for (var i = 0; i < queries.length; i++) {
+        query = queries[i].split(" ");
+        if (query[0] == 'UPDATE' || query[0] == 'Update' || query[0] == 'update') {
+            operation[i] = {
+                'type': 'UPDATE', 
+                'x': query[1], 
+                'y': query[2], 
+                'z': query[3], 
+                'w': query[4]
+            }
+        } else {
+            operation[i] = {
+                'type': 'QUERY', 
+                'x1': query[1], 
+                'y1': query[2], 
+                'z1': query[3], 
+                'x2': query[4],
+                'y2': query[5],
+                'z2': query[6]
+            }
+        }
+    }
+    return operation;
+}
+
+/**
+ *  This object manages the operations of creation of a fenwick matrix, 
+ *  update it with a new weight for a coordinate entered by the user
+ *  as a requested operation and also calculates the summation between 
+ *  two coordinates of the matrix.
+ */
+function fenwick(dimension) {
+    // console.log('dimension: \n',dimension);
+    this.dimension = dimension;
+    this.fenwickMatrix = [];
+    this.update = function(index, val) {
+        // console.log('index: \n',index);
+        // console.log('val: \n',val);
+        if (this.fenwickMatrix.length == 0) {
+            this.fenwickMatrix = createMatrix(this.dimension,this.fenwickMatrix);
+        }
+        for (var row = 0; row < this.fenwickMatrix.length; ++row) {
+            this.fenwickMatrix[row][index] += parseInt(val); 
+            index >>= 1; 
+        } 
+        // console.log('update this.fenwickMatrix: \n',this.fenwickMatrix);
+    }
+    this.summation = function(x1, x2) { 
+        var sum = 0; 
+        for (var row = 0; row < this.fenwickMatrix.length; ++row) { 
+            if (x2&1) {
+                sum += this.fenwickMatrix[row][x2 - 1]; 
+            }
+            x2 >>= 1; 
+        } 
+        return sum; 
+    }
+    function createMatrix(dimension) {
+        var fenwickMatrix = [];
+        while (dimension >= 1) {
+            var arr = [];
+            for (var i = 0; i < dimension; i++) {
+                arr.push(0);
+            }
+            fenwickMatrix.push(arr);
+            dimension >>= 1;
+        }
+        // console.log('createMatrix fenwickMatrix: \n',fenwickMatrix);
+        return fenwickMatrix;
+    };
 }
